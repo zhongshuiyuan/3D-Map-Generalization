42361deebe9f51ec127f9f15683dfa518007d4cd
arslxy <arsrvp@foxmail.com>
2018-09-20 09:52:46 +0800

    准备加入indoor3d室内地图

--- shanghai/js/23D.js
+++ shanghai/js/23D.js
@@ -1,56 +1,36 @@
-//TODO暂未考虑在二三维混搭的模式下缩放时换图层
-var _23DLayerName;
+//二三维混搭，近的三维，远的二维（不显示建筑），中间过渡
+//过渡首先是建筑高度逐渐变为0，再是透明度逐渐变为0
+var _23DLayerName;//当前正在二三维混搭显示的图层名
 document.getElementById("23D").addEventListener("change", function () {
-    if (this.checked===true) {
+    if (this.checked === true) {
+        //TODO判断是否能二三维混搭
         //map.zoomTo(17.1);放得越大二三维混搭效果越明显     
-        // var levelNow = getLevelNow();
-        // var cityNow = "shanghai"//TODO getCityNow();
-        // _23DLayerName = cityNow + "_L" + levelNow;
-        
-        //由于fill-extrusion-opactiy不能data driven styling，用fill来做透明渐变的过渡 TODO放在对应的3d建筑下面
-        // map.addLayer({
-        //     'id': 'opacityTransitionLayer',
-        //     'type': 'fill',
-        //     'source': _23DLayerName,
-        //     'source-layer':_23DLayerName,
-        //     //TODO去获取对应的fill-extrusion图层并动态设置
-        //     'paint': {
-        //         'fill-color': [
-        //             'interpolate',
-        //             ['linear'],
-        //             ['get', 'height'],
-        //             0, 'rgb(255,255,191)',
-        //             75, 'rgb(253,174,97)',
-        //             150, "rgb(215,25,28)",
-        //         ],
-        //     }
-        // })
-
         // var features = map.queryRenderedFeatures({ layers: [_23DLayerName] });
         // if (!features.length) {
         //     alert("当前位置没有显示建筑物，无法实现二三维混搭");
         //     return;
         // }
-
-        changeHeight();
-        map.setPaintProperty(_23DLayerName, "fill-extrusion-height", ["feature-state", "height"]);
-        map.setPaintProperty("opacityTransitionLayer", "fill-opacity", ["feature-state", "opacity"]);
-
-        map.on('move', changeHeight);
+        changeHeightOpacityVisibility();
+        map.on('move', changeHeightOpacityVisibility);
     } else {
         map.removeLayer("opacityTransitionLayer");        
-        map.off('move', changeHeight);
-        _23DDataFirstLoaded = true;
+        map.off('move', changeHeightOpacityVisibility);
+        for (var i = 0; i < layerZoom.length-1; i++) { 
+            map.setFilter("shanghai_L" + (9 - i), null);
+            map.setPaintProperty("shanghai_L" + (9 - i), "fill-extrusion-height", ['get', 'height']);
+        }
     }
 });
 
-//鼠标移动时调整过渡地带的建筑物的高度、透明度 可以改成根据坐标filter来提高速度
-function changeHeight() {
+//地图移动或缩放时，调整过渡地带的建筑物的高度和透明度，隐藏远处建筑 可以改成根据坐标filter来提高速度，前提bearing=0
+function changeHeightOpacityVisibility() {
+    //获取当前显示的三维建筑图层
     var levelNow = getLevelNow();
     var cityNow = "shanghai"//TODO getCityNow();
     _23DLayerName = cityNow + "_L" + levelNow;
 
-    if (map.getLayer("opacityTransitionLayer").source !== _23DLayerName) {//缩放时跨越级别了，要重新加 
+    //第一次要添加透明度渐变的过渡图层，缩放时若跨越级别，要删掉重新添加
+    if (map.getLayer("opacityTransitionLayer")&&map.getLayer("opacityTransitionLayer").source !== _23DLayerName) {
         map.removeLayer("opacityTransitionLayer");
     }
     if (!map.getLayer("opacityTransitionLayer")) { 
@@ -73,49 +53,54 @@ function changeHeight() {
         })
     }    
 
-    var features = map.querySourceFeatures(_23DLayerName, {sourceLayer:_23DLayerName});
-    var _3dFilteredId = ["in", "id"];
-    var _2dFilteredId = ["in", "id"];
-    var uniqueFeatures=getUniqueFeatures(features,'id');
+    //获取当前视窗范围内本应显示的所有建筑
+    var features = map.querySourceFeatures(_23DLayerName, { sourceLayer: _23DLayerName });
+    var uniqueFeatures=getUniqueFeatures(features,'id');    
     
-    //修改geoserver后，仍有少部分要素缺少feature.id
+    //setfeaturestate必须要feature.id，修改geoserver后，仍有少部分要素缺少feature.id
     uniqueFeatures.forEach((feature) => { 
         if (isNaN(feature.id)) { 
             feature.id=feature.properties.id+1;
             console.log(feature);
         }           
     });
+
+    var _3dFilteredId = ["in", "id"];
+    var _2dFilteredId = ["in", "id"];
     
-    //修改featurestate，高度和透明度，隐藏远的建筑
+    //通过featurestate修改每栋建筑的高度和透明度，通过filter隐藏远处的建筑
     uniqueFeatures.forEach((feature) => {
-        var { heightScale, opacityScale } = compute23DScale(feature);
+        var { heightScale, opacity } = compute23DScale(feature);
         if (heightScale > 0) { 
             var height = feature.properties.height * heightScale;
             map.setFeatureState({ source: _23DLayerName, sourceLayer:_23DLayerName, id: feature.id }, { height: height });
             _3dFilteredId.push(feature.properties.id);
         }
-        map.setFeatureState({ source: _23DLayerName, sourceLayer:_23DLayerName, id: feature.id }, { opacity: opacityScale });
-        if (opacityScale > 0) { 
-            _2dFilteredId.push(feature.properties.id);
+        map.setFeatureState({ source: _23DLayerName, sourceLayer:_23DLayerName, id: feature.id }, { opacity: opacity });
+        if (opacity > 0) { 
+            _2dFilteredId.push(feature.properties.id);//本来opactiy为0就已经不显示了，但实测有问题，还是要filter掉
         }
     });
-    //console.log(_3dFilteredId);
+    
     map.setFilter(_23DLayerName, _3dFilteredId);
     map.setFilter("opacityTransitionLayer", _2dFilteredId);
+    map.setPaintProperty(_23DLayerName, "fill-extrusion-height", ["feature-state", "height"]);
+    map.setPaintProperty("opacityTransitionLayer", "fill-opacity", ["feature-state", "opacity"]);
 }
 
-//计算缩放比例
+//计算过渡地带三维建筑物的缩放比例和二维建筑的透明度，依据是建筑到窗口下边沿的距离占整个窗口高度的比例
 function compute23DScale(feature) {
-    var polygon = turf.polygon(feature.geometry.coordinates);
-    var centroid = turf.centroid(polygon).geometry.coordinates;
-    //centroid = [feature.properties.POINT_X, feature.properties.POINT_Y];//用提前处理好的
+    // var polygon = turf.polygon(feature.geometry.coordinates);
+    // var centroid = turf.centroid(polygon).geometry.coordinates;
+    var centroidMercator = [feature.properties.x, feature.properties.y];//用提前处理好的
+    var centroid = toWGS84(centroidMercator);
     var screenPoint = map.project(centroid);
     var scale = 1 - screenPoint.y / map._containerDimensions()[1];
     var heightScale = heightTransition(scale);
     var opacityScale = opacityTransition(scale);
     return {
         heightScale: heightScale,
-        opacityScale:opacityScale
+        opacity:opacityScale
     };
 }
 
@@ -130,7 +115,7 @@ function heightTransition(x) {
     }
 }
 
-//透明度从1过渡到0 TODO应该动态获取3d建筑的透明度，从这个透明度过渡到0
+//透明度从1过渡到0 TODO应该动态获取3d建筑的透明度，从该透明度过渡到0
 function opacityTransition(x) { 
     if (x > 0.6 && x < 0.7) {
         return -10 * x + 7;
@@ -152,7 +137,13 @@ function getUniqueFeatures(array, comparatorProperty) {
     return uniqueFeatures;
 }
 
+//测试，待删除
 map.on('click', 'opacityTransitionLayer', function(e) {
+    var features = map.queryRenderedFeatures(e.point);
+    console.log(features[0]);
+});
+
+map.on('click', 'shanghai_L3', function(e) {
     var features = map.queryRenderedFeatures(e.point);
     console.log(features[0]);
 });
